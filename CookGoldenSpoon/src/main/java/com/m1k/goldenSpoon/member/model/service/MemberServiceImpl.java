afefdd4232609c8e20bc00a86a5a8679e9078c94
FireCurry <firecurry@naver.com>
2023-10-23 21:07:34 +0900

    기본적인거 조금 넣기

---
+++ CookGoldenSpoon/src/main/java/com/m1k/goldenSpoon/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,10 @@
+package com.m1k.goldenSpoon.member.model.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class MemberServiceImpl implements MemberService{
+
+}
