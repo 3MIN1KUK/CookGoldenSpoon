8b018c79cb05fbc472c17d1514a707d9ef2dd324
kkm990708 <kkm990708@gmail.com>
2023-10-23 16:32:19 +0900

    요리의 금수저 프로젝트 등록

---
+++ CookGoldenSpoon/src/main/java/com/m1k/goldenSpoon/CookGoldenSpoonApplication.java
@@ -0,0 +1,14 @@
+package com.m1k.goldenSpoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class CookGoldenSpoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookGoldenSpoonApplication.class, args);
+	}
+
+}
---
+++ CookGoldenSpoon/src/test/java/com/m1k/goldenSpoon/CookGoldenSpoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1k.goldenSpoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookGoldenSpoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
