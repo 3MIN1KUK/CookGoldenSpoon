35cd318b841c3f481b2bd788865241a12c1760bf
kkm990708 <kkm990708@gmail.com>
2023-10-30 02:32:26 +0900

    주말

--- CookGoldenSpoon/src/main/java/com/m1k/goldenSpoon/admin/controller/AdminController.java
+++ CookGoldenSpoon/src/main/java/com/m1k/goldenSpoon/admin/controller/AdminController.java
@@ -11,7 +11,7 @@ public class AdminController {
 	@GetMapping("memberSearch")
 	public String memberSearch() {
 		return "admin/member_search";
-	}
+	}  
 	
 	@GetMapping("detailInquiry")
 	public String detailInquiry() {
@@ -22,7 +22,7 @@ public class AdminController {
 	public String memberDetail() {
 		return "admin/member_detail";
 	}
-	
+	 
 	@GetMapping("commentResult")
 	public String commentResult() {
 		return "admin/comment_result";
