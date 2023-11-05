53f26f4af8b892d7fa8bcda96b55976787e02f57
kkm990708 <kkm990708@gmail.com>
2023-11-06 02:49:04 +0900

    아이디찾기, 비밀번호변경,  로그아웃 구현

--- CookGoldenSpoon/src/main/java/com/m1k/goldenSpoon/member/model/service/MemberService.java
+++ CookGoldenSpoon/src/main/java/com/m1k/goldenSpoon/member/model/service/MemberService.java
@@ -17,4 +17,23 @@ public interface MemberService {
 	 */
 	int signup(Member signupMember);
 
+	/** 아이디 찾기
+	 * @param memberEmail
+	 * @return
+	 */
+	String findId(String memberEmail);
+
+	/** 아이디 , 이메일로 검색
+	 * @param searchMember
+	 * @return
+	 */
+	int findMember(Member searchMember);
+
+	/** 비밀번호 바꾸기
+	 * @param memberNo
+	 * @param memberPw
+	 * @return
+	 */
+	int changePw(int memberNo, String memberPw);
+
 }
